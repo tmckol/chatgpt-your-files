607720e12a467cf9ebaca2f69b51cd2cf55c98a3
Sougata Maity <115576296+tmckol@users.noreply.github.com>
2023-12-29 12:07:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function Index() {
         <div className="flex flex-col items-center mb-4 lg:mb-12">
           <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
           <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center my-12">
-            Dialog Agent for Analgesisa and Relief from Distress</strong> and{' '}
+            <strong>Dialog Agent for Analgesisa and Relief from Distress</strong> and{' '}
             <strong></strong>
           </p>
           {user ? (
